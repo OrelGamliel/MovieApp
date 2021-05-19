deb40b1f435ab11198971fb9fe8b1806b70ace2c
Orel Gamliel <blichen12@gmail.com>
2021-05-19 13:34:20 +0300

    app ready for heroku 2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/movies-rus'));
+app.use(express.static(__dirname + '/dist/moviesRus'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/movies-rus/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/moviesRus/index.html'));
 })
 app.listen(process.env.PORT || 8080);
